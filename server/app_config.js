2674988a9a56e5303250ff1a2c1387f39581f7e4
lukaijie <lukaijie@shopex.cn>
2019-02-18 15:10:33 +0800

    修改config

--- server/app_config.js
+++ server/app_config.js
@@ -2,8 +2,10 @@ module.exports = {
     port: '3000',
     log: 'debug',
     formUrl: 'https://jinshuju.net/api/v1/forms/',
-    appkey: 'c9I8bwNPdY5McXR7bdH02Q',
-    appScrect: 'SoIddEOCUMR9f8cFxk9rDA',
+    // appkey: 'c9I8bwNPdY5McXR7bdH02Q',
+    // appScrect: 'SoIddEOCUMR9f8cFxk9rDA',
+    appkey: '1wItXhgeOEPChFwz5_HvFQ',
+    appScrect: 'a9hfNnJrzsf9xjYMNCF9Dg',
     mysql: {
         host: '39.96.17.179',
         user: 'lzmhlzsd',
