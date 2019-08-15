6af0a18b2a9ae7cb35720ccf1c37b6927b310d4b
lukaijie <lukaijie@shopex.cn>
2019-08-15 10:47:22 +0800

    url

--- server/app_config.js
+++ server/app_config.js
@@ -1,8 +1,8 @@
 module.exports = {
     port: '3000',
     log: 'debug',
-    // formUrl: 'https://jinshuju.net/api/v1/forms/',
-    formUrl: 'https://ntmaimeng.jinshuju.com/api/v1/forms/',
+    formUrl: 'https://jinshuju.net/api/v1/forms/',
+    // formUrl: 'https://ntmaimeng.jinshuju.com/api/v1/forms/',
     // appkey: 'c9I8bwNPdY5McXR7bdH02Q',
     // appScrect: 'SoIddEOCUMR9f8cFxk9rDA',
     // appkey: '1wItXhgeOEPChFwz5_HvFQ',
