b81a07b995e5d8b6bd7e6f896ad7cf18b47b2f99
lukaijie <lukaijie@shopex.cn>
2019-09-12 21:58:13 +0800

    x

--- server/controller/index.js
+++ server/controller/index.js
@@ -18,7 +18,9 @@ module.exports = {
             // 查询数据库中是否已经存在该表单
             const sql = `select * from t_tables where c_id='${formID}'`
             let result = await HELP.sqlExecute( sql )
-            HELP.log( `${logFile} getFormById sql search result: ${JSON.stringify( result )}` )
+            HELP.log(
+                `${logFile} getFormById sql search result: ${JSON.stringify( result )}`
+            )
             if ( result.length > 0 ) {
                 res.send( { ecode: -1, msg: '数据库已经存在该表单' } )
             } else {
@@ -33,7 +35,9 @@ module.exports = {
                 ]
                 data.fields.forEach( m => {
                     for ( var item in m ) {
-                        if ( ['field_1', 'field_2', 'field_3', 'field_4'].indexOf( item ) < 0 ) {
+                        if (
+                            ['field_1', 'field_2', 'field_3', 'field_4'].indexOf( item ) < 0
+                        ) {
                             if ( cField.normal[m[item].type] ) {
                                 tableFields.push( `${item} ${cField.normal[m[item].type].type}` )
                             } else {
@@ -77,9 +81,13 @@ module.exports = {
                     's_c_recode varchar(32)'
                 ]
 
-                const tsql = `create table t_${formID}_unio (${cTableFileds.join( ',' )}, ${tableFields.join( ',' )}, ${saleFileds.join( ',' )})`
+                const tsql = `create table t_${formID}_unio (${cTableFileds.join(
+                    ','
+                )}, ${tableFields.join( ',' )}, ${saleFileds.join( ',' )})`
                 await HELP.sqlExecute( tsql )
-                HELP.log( `${logFile} getFormById table t_${formID}_unio created success`.green )
+                HELP.log(
+                    `${logFile} getFormById table t_${formID}_unio created success`.green
+                )
 
                 // 插入数据库
                 let d1 = JSON.stringify( data.fields )
@@ -87,9 +95,7 @@ module.exports = {
                 let d3 = d2.replace( /\\n/g, '' )
                 let d4 = d3.replace( /\s/g, '' )
                 let d5 = d4.replace( /\"/g, '\\"' )
-                const sql2 = `insert into t_tables (c_id, c_name, c_content, c_desc) values ('${formID}', '${
-                    data.name
-                }', '${d5}', null)`
+                const sql2 = `insert into t_tables (c_id, c_name, c_content, c_desc) values ('${formID}', '${data.name}', '${d5}', null)`
                 await HELP.sqlExecute( sql2 )
                 res.send( { ecode: 0, msg: '操作成功' } )
             }
@@ -114,7 +120,8 @@ module.exports = {
                     pagesize: req.body.pagesize,
                     total: result1[0].count,
                     pageindex: req.body.pageindex
-                } } )
+                }
+            } )
         } catch ( e ) {
             HELP.error( `${logFile} getFormList error: ${e.message}` )
             res.send( { ecode: -1, msg: e.message } )
@@ -157,9 +164,14 @@ module.exports = {
                 } else {
                     if ( HELP.isdatetime( entry[item] ) && item != 'info_os' ) {
                         HELP.log( `${logFile} postFormData datetime ${item}: ${entry[item]}` )
-                        tableValue.push( `'${moment( entry[item] ).format( 'YYYY-MM-DD HH:mm:ss' )}'` )
-                    } else if ( item === 'x_field_weixin_nickname' ) {
-                        tableValue.push( `'nickname'` )
+                        tableValue.push(
+                            `'${moment( entry[item] ).format( 'YYYY-MM-DD HH:mm:ss' )}'`
+                        )
+                    } else if (
+                        item === 'x_field_weixin_nickname' ||
+            item === 'x_field_weixin_province_city'
+                    ) {
+                        tableValue.push( `'null'` )
                     } else {
                         tableValue.push( `'${entry[item]}'` )
                     }
@@ -209,17 +221,7 @@ module.exports = {
                     ]
                 }
             } else {
-                userResultArray = [
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`
-                ]
+                userResultArray = [`''`, `''`, `''`, `''`, `''`, `''`, `''`, `''`, `''`]
             }
 
             let saleResult = []
@@ -242,38 +244,24 @@ module.exports = {
                         `'${saleResult[0].field_10}'`
                     ]
                 } else {
-                    saleResultArray = [
-                        `''`,
-                        `''`,
-                        `''`,
-                        `''`,
-                        `''`,
-                        `''`,
-                        `''`,
-                        `''`
-                    ]
+                    saleResultArray = [`''`, `''`, `''`, `''`, `''`, `''`, `''`, `''`]
                 }
             } else {
-                saleResultArray = [
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`,
-                    `''`
-                ]
+                saleResultArray = [`''`, `''`, `''`, `''`, `''`, `''`, `''`, `''`]
             }
 
             if ( result1[0].count === 0 ) {
                 const unioSql = `insert into t_${tableID}_unio (g_c_name, g_c_grade, g_c_class, g_c_sex, g_c_school,
                     g_c_address, g_c_room, g_c_stuendId, g_c_desc, ${tableField},
                     s_c_leader, s_c_name, s_c_school, s_c_grade, s_c_phone, s_c_qq, s_c_type, s_c_recode) values
-                    (${userResultArray.join( ',' )}, ${tableValue.join( ',' )}, ${saleResultArray.join( ',' )})`
+                    (${userResultArray.join( ',' )}, ${tableValue.join(
+    ','
+)}, ${saleResultArray.join( ',' )})`
                 HELP.log( `unioSql Insert: ${unioSql}`.yellow )
                 await HELP.sqlExecute( unioSql )
-                HELP.log( `${logFile} postFormData t_${tableID}_unio insert success`.green )
+                HELP.log(
+                    `${logFile} postFormData t_${tableID}_unio insert success`.green
+                )
             } else {
                 let setArray = []
                 for ( var sitem in entry ) {
@@ -283,16 +271,21 @@ module.exports = {
                         } else {
                             // setArray.push( `${sitem} = '${entry[sitem]}'` )
                             if ( HELP.isdatetime( entry[sitem] ) && sitem != 'info_os' ) {
-                                HELP.log( `${logFile} postFormData datetime ${sitem}: ${entry[sitem]}` )
-                                setArray.push( `${sitem} = '${moment( entry[sitem] ).format( 'YYYY-MM-DD HH:mm:ss' )}'` )
-                            } else if ( sitem === 'x_field_weixin_nickname' ) {
-                                setArray.push(
-                                    `${sitem} = 'nickname'`
+                                HELP.log(
+                                    `${logFile} postFormData datetime ${sitem}: ${entry[sitem]}`
                                 )
+                                setArray.push(
+                                    `${sitem} = '${moment( entry[sitem] ).format(
+                                        'YYYY-MM-DD HH:mm:ss'
+                                    )}'`
+                                )
+                            } else if (
+                                sitem === 'x_field_weixin_nickname' ||
+                sitem === 'x_field_weixin_province_city'
+                            ) {
+                                setArray.push( `${sitem} = 'null'` )
                             } else {
-                                setArray.push(
-                                    `${sitem} = '${entry[sitem]}'`
-                                )
+                                setArray.push( `${sitem} = '${entry[sitem]}'` )
                             }
                         }
                     }
@@ -308,28 +301,46 @@ module.exports = {
                     `g_c_grade = '${userResult.length > 0 ? userResult[0].c_grade : ''}'`,
                     `g_c_class = '${userResult.length > 0 ? userResult[0].c_class : ''}'`,
                     `g_c_sex = '${userResult.length > 0 ? userResult[0].c_sex : ''}'`,
-                    `g_c_school = '${userResult.length > 0 ? userResult[0].c_school : ''}'`,
-                    `g_c_address = '${userResult.length > 0 ? userResult[0].c_address : ''}'`,
+                    `g_c_school = '${
+                        userResult.length > 0 ? userResult[0].c_school : ''
+                    }'`,
+                    `g_c_address = '${
+                        userResult.length > 0 ? userResult[0].c_address : ''
+                    }'`,
                     `g_c_room = '${userResult.length > 0 ? userResult[0].c_room : ''}'`,
-                    `g_c_stuendId = '${userResult.length > 0 ? userResult[0].c_stuendId : ''}'`,
+                    `g_c_stuendId = '${
+                        userResult.length > 0 ? userResult[0].c_stuendId : ''
+                    }'`,
                     `g_c_desc = '${userResult.length > 0 ? userResult[0].c_desc : ''}'`
                 ]
 
                 let updateSaleResultArray = [
-                    `s_c_leader = '${saleResult.length > 0 ? saleResult[0].field_19 : ''}'`,
+                    `s_c_leader = '${
+                        saleResult.length > 0 ? saleResult[0].field_19 : ''
+                    }'`,
                     `s_c_name = '${saleResult.length > 0 ? saleResult[0].field_1 : ''}'`,
-                    `s_c_school = '${saleResult.length > 0 ? saleResult[0].field_2 : ''}'`,
+                    `s_c_school = '${
+                        saleResult.length > 0 ? saleResult[0].field_2 : ''
+                    }'`,
                     `s_c_grade = '${saleResult.length > 0 ? saleResult[0].field_3 : ''}'`,
                     `s_c_phone = '${saleResult.length > 0 ? saleResult[0].field_4 : ''}'`,
                     `s_c_qq = '${saleResult.length > 0 ? saleResult[0].field_12 : ''}'`,
                     `s_c_type = '${saleResult.length > 0 ? saleResult[0].field_14 : ''}'`,
-                    `s_c_recode = '${saleResult.length > 0 ? saleResult[0].field_10 : ''}'`
+                    `s_c_recode = '${
+                        saleResult.length > 0 ? saleResult[0].field_10 : ''
+                    }'`
                 ]
 
-                const unioSql = `update t_${tableID}_unio set ${updateUserResultArray.join( ',' )},  ${setArray.join( ',' )}, ${updateSaleResultArray.join( ',' )} where serial_number = ${entry.serial_number}`
+                const unioSql = `update t_${tableID}_unio set ${updateUserResultArray.join(
+                    ','
+                )},  ${setArray.join( ',' )}, ${updateSaleResultArray.join(
+                    ','
+                )} where serial_number = ${entry.serial_number}`
                 HELP.log( `unioSql update: ${unioSql}`.yellow )
                 await HELP.sqlExecute( unioSql )
-                HELP.log( `${logFile} postFormData t_${tableID}_unio update success`.green )
+                HELP.log(
+                    `${logFile} postFormData t_${tableID}_unio update success`.green
+                )
             }
             res.send( 200 )
         } catch ( e ) {
@@ -353,47 +364,46 @@ module.exports = {
                 } else {
                     if ( HELP.isdatetime( entry[item] ) && item != 'info_os' ) {
                         HELP.log(
-                            `${logFile} postFromDataSale datetime ${item}: ${
-                                entry[item]
-                            }`
+                            `${logFile} postFromDataSale datetime ${item}: ${entry[item]}`
                         )
                         tableValue.push(
-                            `'${moment( entry[item] ).format(
-                                'YYYY-MM-DD HH:mm:ss'
-                            )}'`
+                            `'${moment( entry[item] ).format( 'YYYY-MM-DD HH:mm:ss' )}'`
                         )
                     } else {
                         tableValue.push( `'${entry[item]}'` )
                     }
                 }
             }
-            const sql1 = `select count(*) as count from t_${tableID} where serial_number = '${
-                entry.serial_number
-            }'`
+            const sql1 = `select count(*) as count from t_${tableID} where serial_number = '${entry.serial_number}'`
             HELP.log( `${logFile} postFromDataSale sql is: ${sql1}` )
             const result1 = await HELP.sqlExecute( sql1 )
-            HELP.log(
-                `select t_${tableID} count: ${JSON.stringify( result1 )}`
-            )
+            HELP.log( `select t_${tableID} count: ${JSON.stringify( result1 )}` )
 
             if ( result1[0].count === 0 ) {
-                const unioSql = `insert into t_${tableID} (${tableField}) values(${tableValue.join( ',' )})`
+                const unioSql = `insert into t_${tableID} (${tableField}) values(${tableValue.join(
+                    ','
+                )})`
                 HELP.log( `unioSql Insert: ${unioSql}`.yellow )
                 await HELP.sqlExecute( unioSql )
-                HELP.log( `${logFile} postFromDataSale t_${tableID} insert success`.green )
+                HELP.log(
+                    `${logFile} postFromDataSale t_${tableID} insert success`.green
+                )
             } else {
                 let setArray = []
                 for ( var sitem in entry ) {
                     if ( sitem !== 'serial_number' ) {
                         if ( entry[sitem] instanceof Object ) {
-                            setArray.push(
-                                `${sitem} = '${JSON.stringify( entry[sitem] )}'`
-                            )
+                            setArray.push( `${sitem} = '${JSON.stringify( entry[sitem] )}'` )
                         } else {
                             // setArray.push( `${sitem} = '${entry[sitem]}'` )
                             if ( HELP.isdatetime( entry[sitem] ) && sitem != 'info_os' ) {
-                                HELP.log( `${logFile} postFromDataSale datetime ${sitem}: ${entry[sitem]}` )
-                                setArray.push( `${sitem} = '${moment( entry[sitem] ).format( 'YYYY-MM-DD HH:mm:ss' )}'`
+                                HELP.log(
+                                    `${logFile} postFromDataSale datetime ${sitem}: ${entry[sitem]}`
+                                )
+                                setArray.push(
+                                    `${sitem} = '${moment( entry[sitem] ).format(
+                                        'YYYY-MM-DD HH:mm:ss'
+                                    )}'`
                                 )
                             } else {
                                 setArray.push( `${sitem} = '${entry[sitem]}'` )
@@ -402,12 +412,13 @@ module.exports = {
                     }
                 }
 
-                const unioSql = `update t_${tableID} set ${setArray.join( ',' )} where serial_number = ${entry.serial_number}`
+                const unioSql = `update t_${tableID} set ${setArray.join(
+                    ','
+                )} where serial_number = ${entry.serial_number}`
                 HELP.log( `unioSql update: ${unioSql}`.yellow )
                 await HELP.sqlExecute( unioSql )
                 HELP.log(
-                    `${logFile} postFromDataSale t_${tableID} update success`
-                        .green
+                    `${logFile} postFromDataSale t_${tableID} update success`.green
                 )
             }
             res.send( 200 )
